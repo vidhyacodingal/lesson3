6dcc155c62966a145ca4459167e654dcad9e8b37
vidhyacodingal <vidhyacodingal@gmail.com>
2024-11-10 19:50:01 +0530

    initial commit

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+a=3
+print("type of a is : ",type(a))
+b = 3.5
+print("type of b is : ",type(b))
+c = "kuku"
+print("type of c is : ",type(c))
+d = True
+print("type of d is : ",type(d))
---
+++ stringop.py
@@ -0,0 +1,3 @@
+text = str(input("enter a text"))
+revtext=text[::-1]
+print(revtext)
